11ae8f9f0f8e2294c31d8a180f35df24c7d8de69
wingblaze <paolovergara2004@yahoo.com>
2014-08-15 02:43:58 +0800

    Drawing card with verifications using try catch

--- src/com/vergara/mp/Player.java
+++ src/com/vergara/mp/Player.java
@@ -1,116 +1,151 @@
 package com.vergara.mp;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Stack;
 
 import com.vergara.mp.card.Card;
 import com.vergara.mp.card.InvestmentCard;
 import com.vergara.mp.card.property.PropertyCard;
 
-public class Player 
-{
+public class Player {
 	private String Name;
 	private ArrayList<Card> Hand;
-	private ArrayList<Card> Deck;
+	private Stack<Card> Deck;
 	private int Cash;
 	private Field field;
-	
-	public String getName() 
-	{
-		return Name;
-	}
-	public void setName(String name) 
-	{
+	private int maxSpend;
+	private int currentSpend;
+
+	public Player(String name) {
 		Name = name;
 	}
-	public ArrayList<Card> getHand() 
-	{
+
+	public String getName() {
+		return Name;
+	}
+
+	public ArrayList<Card> getHand() {
 		return Hand;
 	}
-	public void setHand(ArrayList<Card> hand) 
-	{
-		Hand = hand;
-	}
-	public ArrayList<Card> getDeck() 
-	{
+
+	public Stack<Card> getDeck() {
 		return Deck;
 	}
-	public void setDeck(ArrayList<Card> deck) 
-	{
-		Deck = deck;
-	}
-	public int getCash()
-	{
+
+	public int getCash() {
 		return Cash;
 	}
-	public void setCash(int cash)
-	{
-		Cash = cash;
-	}
-	
+
 	/**
 	 * Ends the current players' turn.
 	 */
-	public void endTurn()
-	{
-		
+	public void endTurn() {
+
 	}
+
 	/**
 	 * Draw card function.
 	 */
-	public ArrayList<Card> investOpportunity()
-	{
+	public ArrayList<Card> investOpportunity() {
+
+		try {
+			double chance = Math.random();
+
+			if (Cash < 1000)
+				throw new Exception("You don't have enough money.");
+
+			if (Deck.size() == 0)
+				throw new Exception("You don't have enough cards in your deck.");
+
+			if (currentSpend > maxSpend)
+				throw new Exception("You are spending too much.");
+
+			// Checks if Player has enough money.
+			// Checks for 10% chance
+			if (chance > 0.9) {
+				System.out
+						.println("Investing in Opportunities cost 1000PHP. You draw 2 cards! Double Opportunity!");
+
+				// Get 2 cards
+				Card Card1 = Deck.pop();
+				Card Card2 = Deck.pop();
+
+				// Pay for draw
+				Cash -= 1000;
+
+				Hand.add(Card1);
+				Hand.add(Card2);
+
+			} else {
+				System.out
+						.println("Investing in Opportunities cost 1000PHP. You draw 1 card");
+
+				// Get 1 card
+				Card Card1 = Deck.pop();
+
+				// Pay for draw
+				Cash -= 1000;
+
+				Hand.add(Card1);
+
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 		ArrayList<Card> drawnCards = new ArrayList<Card>();
-		
+
 		// draw from the deck.remove()
-		
-		return drawnCards ;	
+
+		return drawnCards;
 	}
+
 	/**
 	 * Play Investment Card onto field from the hand.
 	 */
-	public void purchaseInvestment(InvestmentCard card)
-	{
+	public void purchaseInvestment(InvestmentCard card) {
 		// subtract from player cash based on price of the card
 		// Cash -= c.getPrice()
 		Hand.remove(card);
 		field.useInvestment(card);
 	}
+
 	/**
 	 * Invests on property card selected
 	 */
-	public void investProperty(PropertyCard primeInvestment)
-	{
+	public void investProperty(PropertyCard primeInvestment) {
 		// checks if primeInvestment
 	}
+
 	/**
-	 * Makes selected property card on asset/property field into the prime investment.
+	 * Makes selected property card on asset/property field into the prime
+	 * investment.
 	 */
-	public void setPrimeInvestment(PropertyCard card)
-	{
+	public void setPrimeInvestment(PropertyCard card) {
 		// check if empty; else switch places
 		ArrayList<InvestmentCard> investmentSpace = field.getInvestmentSpace();
 		int index = investmentSpace.indexOf(card);
 		investmentSpace.remove(card);
-		
+
 		PropertyCard insert = field.getPrimeInvestment();
 		investmentSpace.add(index, insert);
-		
+
 		field.setPrimeInvestment(card);
 	}
+
 	/**
 	 * Sells the selected Property or Asset Card
 	 */
-	public void sellInvestment(InvestmentCard card)
-	{
+	public void sellInvestment(InvestmentCard card) {
 		field.removeInvestment(card);
-		//get some cash based on card selling price
+		// get some cash based on card selling price
 	}
+
 	/**
 	 * Uses the event card selected
 	 */
-	public void useEvent()
-	{
-		
+	public void useEvent() {
+
 	}
 }
-
